3f6428dad05541b7ad43700956b0c8d9bef1621b
DIEGO CAMILO MARTINS VIEIRA <dcvieira@tjsp.jus.br>
2022-09-19 01:01:45 -0300

    first commit

---
+++ lib/pages/catalogo/catalogo-appbar.dart
@@ -0,0 +1,33 @@
+import 'package:badges/badges.dart';
+import 'package:cart_provider_demo/models/carrinho.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CatalogoAppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    print('BUILD: CatalogoAppBar');
+
+    final carrinho = context.watch<CarrinhoModel>();
+    return AppBar(
+      title: const Text('Catalogo'),
+      actions: [
+        IconButton(
+          icon: Badge(
+            badgeContent: Text(
+              '${carrinho.items.length}',
+              style: TextStyle(color: Colors.white),
+            ),
+            child: const Icon(
+              Icons.shopping_cart,
+            ),
+          ),
+          onPressed: () => Navigator.pushNamed(context, '/carrinho'),
+        )
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(60.0);
+}
